b4e912a7108de0a2a85e2df701fd301c276f03ad
YavorMarkov <76599518+YavorMarkov@users.noreply.github.com>
2023-05-26 17:28:06 +0300

    Update aws-terraform-s3-example.tf

--- aws-terraform-s3-example.tf
+++ aws-terraform-s3-example.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "bucket" {
-  bucket = "terraform-example-bucket"
+  bucket = "terraform-example-bucket-1234557985122547698754324w"
   acl    = "private"
   
   tags = {
